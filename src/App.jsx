6c9a9ee723134ff74d2c2c5e847e92be0fb7f499
Rameshwar_Sahu <rameshwarsahu627@gmail.com>
2025-06-11 00:17:10 +0530

    Important comments added

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   }
   const handleDelete = ()=>{
-
+    let id = e.ta
   }
   const handleAdd = ()=>{
     // Generate unique uuid : npm i uuid
@@ -20,17 +20,21 @@ function App() {
   const handlechange = (e)=>{
     setTodo(e.target.value)
   }
-  // const handleCheckbox = (e) => {
-  //   e.target.isCompleted = !(e.target.isCompleted)
-  //   e.target.todo.style.text-decoration-line = line-through
-  // }
   const handleCheckbox = (id) => {
-  setTodos(todos =>
-    todos.map(todo =>
-      todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo
-    )
-  );
-};
+    // We need a useCallback(guiding func) inside findindex
+    let index = todos.findIndex(item=>{
+      return item.id === id;
+    })
+    let newTodos = [...todos]; // Shallow copy
+    newTodos[index].isCompleted = !newTodos[index].isCompleted;// togglig rqd todo
+    setTodos(newTodos)
+  }
+  // const handleCheckbox = (id) => {
+  // setTodos((todos) =>
+  //   todos.map(todo =>
+  //     todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo
+  //   ));
+  // };
   
 
   return (
@@ -42,11 +46,13 @@ function App() {
           <input onChange={handlechange} value={todo} type="text" className='w-1/2 bg-amber-50'/>
             <button onClick={handleAdd} className='cursor-pointer bg-violet-700 hover:bg-violet-900 px-4 py-1 text-sm font-bold text-white rounded-md mx-6' >Add</button>
         </div>
+        
         <h2 className='text-lg font-bold'>Your Todos</h2>
+        
         <div className="todos items-center flex flex-col gap-2 justify-center my-3">
           {todos.map(item=>{
           return <div key={item.id} className='todo flex w-1/2 justify-between'>
-            {/* <input onChange={handleCheckbox(item.id)} type="checkbox" checked={item.isCompleted} name={item.id} /> */}
+            {/* To pass something arrow function is needed */}
             <input onChange={() => handleCheckbox(item.id)} type="checkbox" checked={item.isCompleted} name={item.id}/>
             <div className={item.isCompleted?"line-through":""}>{item.todo}</div>
             <div className="buttons flex align-middle gap-5">
