591eb1461ed1041254b85c32e69171843e65aa6b
Rameshwar_Sahu <rameshwarsahu627@gmail.com>
2025-06-11 16:37:27 +0530

    Finally Designed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className="flex justify-between bg-blue-700 text-white px-8 py-2">
+    <nav className="flex justify-between backdrop-blur-md bg-[rgba(0,0,0,0.19)]  text-white px-8 py-2 shadow-md">
         <div className="logo">
             <span className='font-bold' >
                 iTask
             </span>
         </div>
         <ul className="flex justify-between gap-8">
-            <li className='cursor-pointer hover:font-bold transition-all duration-75'>Home</li>
-            <li className='cursor-pointer hover:font-bold transition-all duration-75'>Tasks</li>
+            <li className='cursor-pointer hover:scale-110 transition-all duration-75'>Home</li>
+            <li className='cursor-pointer hover:scale-110 transition-all duration-75'>Tasks</li>
         </ul>
     </nav>
   )
